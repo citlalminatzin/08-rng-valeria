d1f8a5d2c236658a6c92ca57ac76e78e5bdc60c0
Valeria Lazcano <valelf2005@gmail.com>
2026-04-24 13:51:54 -0600

    prueba

--- monty-hall.py
+++ monty-hall.py
@@ -14,7 +14,7 @@ def play_change(n:int = 1000) -> float:
         elec_inicial = random.randint(0, 2)
         puertas = [i for i in range(3)
         if i != ganador and i != elec_inicial]
-        abierto = random.select(puertas)
+        abierto = random.choices(puertas)
     
         nueva_opcion = [i for i in range(3)
         if i != elec_inicial and i != abierto][0]
@@ -37,10 +37,14 @@ def play_stay(n:int = 1000)->float:
     return exito/n
 
 def main():
-    success_change = play_change()
-    success_stay   = play_stay()
-    print(f"{success_change=} {success_stay=}")
 
+    n = 10000
+    success_change = play_change(n)
+    success_stay = play_stay(n)
+    
+    print(f"--- Resultados de la Misión Pescado ({n} intentos) ---")
+    print(f"Tasa de éxito si el gato CAMBIA: {success_change:.2%}")
+    print(f"Tasa de éxito si el gato SE QUEDA: {success_stay:.2%}")
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     main()
