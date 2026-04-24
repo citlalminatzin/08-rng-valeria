685602530e765a328eca52f8e9233a4685dcda17
Valeria Lazcano <valelf2005@gmail.com>
2026-04-24 13:46:53 -0600

    monty-hall.py

--- monty-hall.py
+++ monty-hall.py
@@ -1,17 +1,40 @@
-
+import random
 def create_game()->int:
     """Escoge la puerta ganadora"""
-    ...
+    return random.randint(0, 2)
 
 def play_change(n:int = 1000) -> float:
     """
     Juega monty-hall con la estrategia de cambiar la puerta
     Regresa la tasa de éxito
     """
-    ...
+    exito = 0
+    for _ in range(n):
+        ganador = create_game()
+        elec_inicial = random.randint(0, 2)
+        puertas = [i for i in range(3)
+        if i != ganador and i != elec_inicial]
+        abierto = random.select(puertas)
+    
+        nueva_opcion = [i for i in range(3)
+        if i != elec_inicial and i != abierto][0]
+        
+        if nueva_opcion == ganador:
+            exito += 1
+            
+    return exito/n
+    
 def play_stay(n:int = 1000)->float:
     """Juega monty-hall con la estrategia de NO cambiar la puerta"""
-    ...
+    exito = 0
+    for _ in range(n):
+        ganador = create_game()
+        elec_inicial = random.randint(0, 2)
+    
+        if elec_inicial == ganador:
+            exito += 1
+            
+    return exito/n
 
 def main():
     success_change = play_change()
